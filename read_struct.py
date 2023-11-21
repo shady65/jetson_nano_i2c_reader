092c9e5f99343b3735d6af6e3537681c7906ef7b
shady65 <shadyosama651999@gmail.com>
2023-11-21 17:17:59 +0100

    edit_struct_size

--- read_struct.py
+++ read_struct.py
@@ -4,7 +4,7 @@ import time
 
 # Define the I2C address of the Arduino
 arduino_address = 8  # Make sure it matches the Arduino's I2C address
-
+struct_size = 40
 # Define the struct format to match the Arduino's struct
 struct_format = 'ff ffff ff Q'
 
@@ -13,9 +13,9 @@ def read_sensor_data():
     try:
         # Open the I2C bus
         bus = smbus.SMBus(1)  # 1 indicates the I2C bus number on the Jetson Nano
-
+        print('bus is opened')
         # Read the struct from Arduino
-        raw_data = bus.read_i2c_block_data(arduino_address, 0, 32)  # 32 is the size of the struct in bytes
+        raw_data = bus.read_i2c_block_data(arduino_address, 0, struct_size)  # 32 is the size of the struct in bytes
 
         # Unpack the received data into the SensorData struct
         sensor_data = struct.unpack(struct_format, bytearray(raw_data))
