623f43a897b7a71d178ea453ba96885210f9bbf2
shady65 <shadyosama651999@gmail.com>
2023-11-22 00:32:18 +0100

    can read the struct

--- read_struct.py
+++ read_struct.py
@@ -4,9 +4,9 @@ import time
 
 # Define the I2C address of the Arduino
 arduino_address = 3*16 # Make sure it matches the Arduino's I2C address
-struct_size = 40
+struct_size = 28
 # Define the struct format to match the Arduino's struct
-struct_format = 'ff ffff ff Q'
+struct_format = 'fffffff'
 
 
 # Function to read the SensorData struct from Arduino
@@ -16,21 +16,16 @@ def read_sensor_data():
         bus = smbus.SMBus(1)  # 1 indicates the I2C bus number on the Jetson Nano
         print('bus is opened')
         # Read the struct from Arduino
-        #raw_data = bus.read_i2c_block_data(arduino_address, 0, struct_size)  # 32 is the size of the struct in bytes
-        # Read a single byte from Arduino
-        char_data = bus.read_byte(arduino_address)
+        raw_data = bus.read_i2c_block_data(arduino_address, 0, struct_size)  
+        print(len(raw_data))
+   
+        # Unpack the received data into the SensorData struct
+        sensor_data = struct.unpack(struct_format, bytearray(raw_data))
 
-        # Close the I2C bus
+         # Close the I2C bus
         bus.close()
 
-        return chr(char_data)
-#         # Unpack the received data into the SensorData struct
-#         sensor_data = struct.unpack(struct_format, bytearray(raw_data))
-# #comment
-#         # Close the I2C bus
-#         bus.close()
-
-#         return sensor_data
+        return sensor_data
 
     except Exception as e:
         print(f"Error: {e}")
@@ -40,18 +35,16 @@ def read_sensor_data():
 while True:
     # Read sensor data from Arduino
     data = read_sensor_data()
-    print('data')
-    print(type(data))
-    print(data + 'x')
-    print("Received Character (ASCII):", ord(data))
-    print("Received Character (repr):", repr(data))
-    # if data is not None:
-    #     # Print the received sensor data
-    #     print("GPS Latitude:", data[0])
-    #     print("GPS Longitude:", data[1])
-    #     print("IMU Acc (X, Y, Z):", data[2], data[3], data[4])
-    #     print("IMU Gyro:", data[5])
-    #     print("Ultrasonic (1, 2):", data[6], data[7])
-    #     print("Timestamp:", data[8])
+
+    if data is not None:
+        # Print the received sensor data
+        for i in data:
+           print("reading:", repr(i))
+
+        print("GPS Longitude:", data[0])
+        print("IMU Acc (X, Y, Z):", data[1], data[2], data[3])
+        print("IMU Gyro:", data[4])
+        print("Ultrasonic (1, 2):", data[5], data[6])
+
 
     time.sleep(1)  # Adjust the delay as needed
